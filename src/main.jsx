fa0a3bd756275f2fa1b200425558f7299e55d6a0
lamwj98 <58262104+lamwj98@users.noreply.github.com>
2024-03-21 16:45:41 +0800

    bug: fix hide sidebar (#10)
    * fix: hide sidebar
    
    * ci: test
    
    * ci: test
    
    * ci: test
    
    * ci: test
    
    * ci: test
    
    * ci: test
    
    * ci :test
    
    * ci: test
    
    * ci :test
    
    * ci: test
    
    * chore(release): 2.0.18 [skip ci]
    
    ## [2.0.18](https://github.com/sg-developer-portal/docsify-sidebar-collapse/compare/v2.0.17...v2.0.18) (2024-03-20)
    
    ### Bug Fixes
    
    * hide sidebar ([5de9096](https://github.com/sg-developer-portal/docsify-sidebar-collapse/commit/5de90963c4909cf5c00b05dc55c6a7529e59b9a8))
    
    * ci: test
    
    * ci: test
    
    * ci: test
    
    * Delete CHANGELOG.md
    
    * ci: test
    
    * ci: test
    
    * ci: test
    
    * ci: test
    
    * chore(release): 2.0.22 [skip ci]
    
    ## [2.0.22](https://github.com/sg-developer-portal/docsify-sidebar-collapse/compare/v2.0.21...v2.0.22) (2024-03-20)
    
    ### Bug Fixes
    
    * hide sidebar ([5de9096](https://github.com/sg-developer-portal/docsify-sidebar-collapse/commit/5de90963c4909cf5c00b05dc55c6a7529e59b9a8))
    
    * ci: test
    
    * ci: test
    
    * ci: test
    
    * chore(release): 2.0.22 [skip ci]
    
    ## [2.0.22](https://github.com/sg-developer-portal/docsify-sidebar-collapse/compare/v2.0.21...v2.0.22) (2024-03-20)
    
    ### Bug Fixes
    
    * hide sidebar ([5de9096](https://github.com/sg-developer-portal/docsify-sidebar-collapse/commit/5de90963c4909cf5c00b05dc55c6a7529e59b9a8))
    
    * ci: test
    
    * chore(release): 2.0.22 [skip ci]
    
    ## [2.0.22](https://github.com/sg-developer-portal/docsify-sidebar-collapse/compare/v2.0.21...v2.0.22) (2024-03-20)
    
    ### Bug Fixes
    
    * hide sidebar ([5de9096](https://github.com/sg-developer-portal/docsify-sidebar-collapse/commit/5de90963c4909cf5c00b05dc55c6a7529e59b9a8))
    
    * ci: test
    
    * chore(release): 2.0.22 [skip ci]
    
    ## [2.0.22](https://github.com/sg-developer-portal/docsify-sidebar-collapse/compare/v2.0.21...v2.0.22) (2024-03-20)
    
    ### Bug Fixes
    
    * hide sidebar ([5de9096](https://github.com/sg-developer-portal/docsify-sidebar-collapse/commit/5de90963c4909cf5c00b05dc55c6a7529e59b9a8))
    
    * ci: test
    
    * ci: test
    
    * ci: test
    
    * ci:test
    
    * ci: test
    
    ---------
    
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

--- src/main.jsx
+++ src/main.jsx
@@ -5,23 +5,37 @@ import "./assets/main.scss";
 
 window.$docsify = window.$docsify || {};
 window.$docsify.plugins = [].concat(function(hook, vm) {
-    const { enableSidebarCollapse, nameLink: basePath } = vm.config;
+    const { enableSidebarCollapse, nameLink: basePath, hideSidebar } = vm.config;
 
     if (!enableSidebarCollapse) return;
 
+
+
     hook.mounted(() => {
-        const rootElement = document.querySelector(".sidebar-nav");
+        if (hideSidebar) {
+            // FIXME : better styling solution
+            [
+              document.querySelector('aside.sidebar'),
+              document.querySelector('button.sidebar-toggle'),
+            ].forEach(node => node.parentNode.removeChild(node));
+            document.querySelector('section.content').style.right = 'unset';
+            document.querySelector('section.content').style.left = 'unset';
+            document.querySelector('section.content').style.position = 'relative';
+            document.querySelector('section.content').style.width = '100%';
+        } else {
+            const rootElement = document.querySelector(".sidebar-nav");
 
-        // Clear and flag the element
-        rootElement.innerHTML = ""
-        rootElement.id = "enhanced-sidebar";
-        rootElement.className = "enhanced-sidebar";
-
-        vm._renderSidebar = (markdown) => {
-            const lexer = new Lexer();
-            const tokens = lexer.lex(markdown);
-
-            render(<Sidebar tokens={tokens} basePath={basePath} />, rootElement);
+            // Clear and flag the element
+            rootElement.innerHTML = ""
+            rootElement.id = "enhanced-sidebar";
+            rootElement.className = "enhanced-sidebar";
+    
+            vm._renderSidebar = (markdown) => {
+                const lexer = new Lexer();
+                const tokens = lexer.lex(markdown);
+    
+                render(<Sidebar tokens={tokens} basePath={basePath} />, rootElement);
+            }
         }
     })
 }, $docsify.plugins || []);
