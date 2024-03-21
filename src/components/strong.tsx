1b4015883c6398ac194e86422f797a6e37ff50bd
Luth Andyka <210670b@mymail.nyp.edu.sg>
2024-03-21 14:25:03 +0800

    render text only for em and strong tokens

--- src/components/strong.tsx
+++ src/components/strong.tsx
@@ -1,17 +1,10 @@
 import { Tokens } from "marked";
-import { ComponentProps, JSX } from "preact";
-import Token from "./token";
+import { ComponentProps } from "preact";
 
 type StrongProps = {
   token: Tokens.Strong;
 } & ComponentProps<"strong">;
 
 export default function Strong({ token, ...props }: StrongProps) {
-  let el: JSX.Element[] | undefined = undefined;
-  if (token.tokens) {
-    el = token.tokens.map((t, idx) => (
-      <Token token={t} key={`token-${token.type}-${token.text}-${idx}`} />
-    ));
-  }
-  return <strong {...props}>{el || token.text}</strong>;
+  return <strong {...props}>{token.text}</strong>;
 }
